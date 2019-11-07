366d14f5778cee346458101d245b74a9fd37a338
izanami0511 <murkek4@mail.ru>
2019-11-07 21:25:02 +0300

    Test

--- main.cpp
+++ main.cpp
@@ -1,18 +1,5 @@
 #include <iostream>
 #include <Windows.h>
-<<<<<<< HEAD
-#include "human.h"
-
-using namespace std;
-
-int main () {
-	Human b;
-	b.add_el();
-	b.Print();
-	cout << "********************";
-	b.add_el();
-	b.Print();
-=======
 #include "Keeper.h"
 
 using namespace std;
@@ -25,7 +12,6 @@ int main() {
 	cout << "---------------------------------------" << endl;
 	b1->addObject(b);
 	b1->print();
->>>>>>> New Fixes
 	system("pause");
 	return 0;
 }
